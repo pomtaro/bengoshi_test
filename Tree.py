7d18ad571539cd4883c0266d768312dde7ae67de
HigashiTakanori_LS <pomtaro0509@gmail.com>
2019-02-08 00:30:55 +0900

    pic_bot 29

--- Tree.py
+++ Tree.py
@@ -175,7 +175,7 @@ class Tree():
     def decide_images(self, indexes):
         if len(indexes) == 1:
             images = self.layers_images[len(indexes)][indexes[0]]
-            return buttons
+            return images
         elif len(indexes) == 2:
             images = self.layers_images[len(indexes)][indexes[0]][indexes[1]]
             return images
