44535e3cd81ad9ac72f333b9a02913b45d38e46c
HigashiTakanori_LS <pomtaro0509@gmail.com>
2019-02-19 23:58:17 +0900

    flow 25

--- bengoshi_dev_flow.py
+++ bengoshi_dev_flow.py
@@ -2,43 +2,25 @@
 import os
 import sys
 import json
-from datetime import datetime
 import time
-from Tree import Tree
-from Tree import Guidance
-
-from Flow import Flow
-
 import requests
 from flask import Flask, request
+from datetime import datetime
+from Flow import Flow
 
 app = Flask(__name__)
 
 ACCESS_TOKEN = 'EAAHZBLbwxSdQBAD2W7I4HWqobjl3WavuH1ZBw0PLE98L6lnYZCRrfVr9YVkzuNgDwl2LwavCeh26jrwthM4yb3nZAxPHkDMqBHXRRzEC4VM1koJmSsCHiJ4PROBprossElEBpvnb9H3BFrBZCqNmyIn656mOmOK1gKCsUAJXVhwZDZD'
 VERIFY_TOKEN = 'Verify_Token_Dev'
 
-# 弁護士bot_テスト用：EAAHZBLbwxSdQBAGPjgg3IdGTl76jRSZAfHjjjhuq1B3vTHsfczRwMnHoZBEZADRa7Ui4fYNuVMO5aZCza4K582yQwo0gmV8sojNplVW0PGokf06QavX56sqpfC2PemspkASAo8sTPZC85vt12UpQvg0HxExAmffeKf27cvKGfrTwZDZD
-
-view_count = 0
-info_step = 0
-
-customer_name = ''
-customer_name_sub = ''
-customer_address = ''
-customer_number = ''
-
 lawyer_id = '2230490113648972'  # こじま'2316784358340526'
 
-#  ツリークラスを初期化
-tree = Tree()
-#  ガイダンスクラスを初期化
-guidance = Guidance()
-#  フロークラスを初期化
+# Flowクラスの初期化
 flow = Flow()
 
 def send_get_started():
     params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
+        "access_token": ACCESS_TOKEN
     }
     headers = {
         "Content-Type": "application/json"
@@ -51,9 +33,9 @@ def send_get_started():
             {
                 "locale": "default",
                 "text": "初めまして！😄\n"
-                        "離婚お悩み相談室の福郎だよ！\n"
-                        "ぼくが出す選択肢から選ぶだけでお悩みを具体化して、\n"
-                        "さらにあまり知られていない豆知識もおしえてあげるよ！👍\n"
+                        "ぼくは借金セルフチェックアシスタントの福郎だよ！\n"
+                        "借金にはとても大きな精神的苦痛があるけど、\n"
+                        "借金を返済する人を支援する仕組みがちゃんとあるんだ！\n"
                         "\n"
                         "一人で悩まず、まずはぼくと話してみない？🤔"
             }
@@ -99,125 +81,14 @@ def webhook():
                     if messaging_event["message"].get("text"):
                         message_text = messaging_event["message"]["text"]  # the message's text
 
-
                         # インジケータ
                         send_typing_on(sender_id)
 
+                        # botの本体
                         flow.execute_method(sender_id, message_text, ACCESS_TOKEN)
 
-
-                        """
-                        # テキストサーチ
-                        indexes = guidance.search_text(message_text)
-
-                        # テキストサーチ結果が存在するか
-                        if not indexes == None:
-                            if indexes == [0, 0]:  # 役に立った！
-                                # ボットテキストfirst
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-
-                                # ボットテキストsecond
-                                text_second = guidance.decide_text_second(indexes)
-                                send_message(sender_id, text_second)
-
-                                # ボタンレスカルーセル送信
-                                titles, image_urls, subtitles = guidance.decide_images(indexes)
-                                send_carousel_buttonless(sender_id, titles, image_urls, subtitles)
-
-                                # クイック返信送信
-                                text_third = guidance.decide_text_third(indexes)
-                                buttons = guidance.decide_buttons(indexes)
-                                send_quick_reply(sender_id, text_third, buttons)
-
-                            elif indexes == [1, 0]:  # ホームページを見る！
-                                # ボットテキストfirst
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-
-                                # ボットテキストsecond
-                                text_second = guidance.decide_text_second(indexes)
-                                send_message(sender_id, text_second)
-
-                                title = 'さくら総合法律事務所'
-                                subtitle = '武藤洋善 弁護士\n「ご相談内容をよく聞き、懇切、丁寧に」をモットーにしています。'
-                                url_str = 'http://sakurasogo-law.jp/member/mutou.html'
-                                image_url = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E5%BC%81%E8%AD%B7%E5%A3%AB%E5%86%99%E7%9C%9F/%E6%AD%A6%E8%97%A4%E3%81%95%E3%82%93%E5%86%99%E7%9C%9F.png'
-                                send_url_image(sender_id, title, subtitle, url_str, image_url)
-                                text_third = guidance.decide_text_third(indexes)
-                                buttons = guidance.decide_buttons(indexes)
-                                send_quick_reply(sender_id, text_third, buttons)
-
-                            elif indexes == [2, 0]:  # 連絡してみる！
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-                                text_second = guidance.decide_text_second(indexes)
-                                send_message(sender_id, text_second)
-                                text_third = guidance.decide_text_third(indexes)
-                                buttons = guidance.decide_buttons(indexes)
-                                send_quick_reply(sender_id, text_third, buttons)
-
-                            elif indexes == [3, 0] or indexes == [4, 1]:  # OK1
-                                indexes = [3, 0]
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-                                info_step = 1
-
-                            elif indexes == [4, 0]:  # OK2
-                                text_first = guidance.decide_text_first(indexes)
-                                buttons = guidance.decide_buttons(indexes)
-                                send_quick_reply(sender_id, text_first, buttons)
-                            elif indexes == [5, 0]:  # 送信する
-                                content = 'テストコンテンツ'
-                                send_info_to_lawyer(lawyer_id, customer_name, customer_name_sub, customer_address, customer_number, content)
-                                send_ok_ng_buttons(lawyer_id, sender_id)
-
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-                                text_second = guidance.decide_text_second(indexes)
-                                send_message(sender_id, text_second)
-                                text_third = guidance.decide_text_third(indexes)
-                                send_message(sender_id, text_third)
-                            else:
-                                text_first = guidance.decide_text_first(indexes)
-                                send_message(sender_id, text_first)
-                                text_second = guidance.decide_text_second(indexes)
-                                send_message(sender_id, text_second)
-                                url = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E8%A3%9C%E8%B6%B3%E8%AA%AC%E6%98%8E%E7%94%A8/%E3%83%9C%E3%82%BF%E3%83%B3%E8%AA%AC%E6%98%8E.png'
-                                send_image(sender_id, url)
-                                text_third = guidance.decide_text_third(indexes)
-                                send_message(sender_id, text_third)
-
-
-                        else:
-                            if info_step == 1:
-                                customer_name = message_text
-                                text_second = guidance.decide_text_second([3, 0])
-                                send_message(sender_id, text_second)
-                                info_step = 2
-                            elif info_step == 2:
-                                customer_name_sub = message_text
-                                text_third = guidance.decide_text_third([3, 0])
-                                send_message(sender_id, text_third)
-                                info_step = 3
-                            elif info_step == 3:
-                                customer_address = message_text
-                                text_fourth = guidance.decide_text_fourth([3, 0])
-                                send_message(sender_id, text_fourth)
-                                info_step = 4
-                            elif info_step == 4:
-                                customer_number = message_text
-                                send_info_validation(sender_id)
-                                info_step = 0
-                            else:
-                                text = 'エラー'
-                                send_message(sender_id, text)
-                        """
-
                     else:
-                        text = "すみません、もう一度選択してください。"
-                        buttons = ["労働について", "離婚・男女問題について", "借金について"]
-                        send_quick_reply(sender_id, text, buttons)
+                        pass
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -231,145 +102,13 @@ def webhook():
                     message_text = messaging_event["postback"]["title"]
                     return_id = messaging_event["postback"]["payload"]
 
+                    # インジケータ
                     send_typing_on(sender_id)
 
+                    # botの本体
                     flow.execute_method(sender_id, message_text, ACCESS_TOKEN)
 
-
-
-
-                    """
-                        url_startimage = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%88/%E3%83%95%E3%82%AF%E3%83%AD%E3%82%A6_test.png'
-                        send_image(sender_id, url_startimage)
-
-                        text = 'はじめまして😄\nぼくは離婚についてアドバイスする福郎だよ！'
-                        send_message(sender_id, text)
-
-                        text = 'まずは気になる項目を下から選んでみてね👍'
-                        send_message(sender_id, text)
-
-                        titles = ["性格の不一致", "浪費、借金", "DV", "浮気、男女問題", "その他"]
-
-                        mismatch_of_personality = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E6%80%A7%E6%A0%BC%E3%81%AE%E4%B8%8D%E4%B8%80%E8%87%B4/%E6%80%A7%E6%A0%BC%E3%81%AE%E4%B8%8D%E4%B8%80%E8%87%B42.png'
-                        waste_debt = 'https://github.com/pomtaro/bengoshi_test/blob/master/pic_bot/%E6%B5%AA%E8%B2%BB%E3%83%BB%E5%80%9F%E9%87%91/%E6%B5%AA%E8%B2%BB%E3%83%BB%E5%80%9F%E9%87%91.png?raw=true'
-                        dv = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/DV/DV2.png'
-                        unfaithful_sexualproblem = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E6%B5%AE%E6%B0%97%E3%83%BB%E7%94%B7%E5%A5%B3%E5%95%8F%E9%A1%8C/%E6%B5%AE%E6%B0%97%E3%83%BB%E7%94%B7%E5%A5%B3%E5%95%8F%E9%A1%8C.png'
-                        other = 'https://raw.githubusercontent.com/pomtaro/bengoshi_test/master/pic_bot/%E3%81%9D%E3%81%AE%E4%BB%96/%E3%81%9D%E3%81%AE%E4%BB%96.png'
-
-                        image_urls = [mismatch_of_personality, waste_debt, dv, unfaithful_sexualproblem, other]
-                        subtitles = ["性格の不一致", "浪費、借金", "DV", "浮気、男女問題", "その他"]
-                        buttons_titles = [["性格の不一致"], ["浪費、借金"], ["DV"], ["浮気、男女問題"], ["その他"]]
-                        send_carousel(sender_id, titles, image_urls, subtitles, buttons_titles)
-                    """
-
-
-
-
-
-                    if message_text == "NG":
-                        text = '大変申し訳ございません。今回はお客様のご相談にお応えできる弁護士が見つかりませんでした。'
-                        send_message(return_id, text)
-                        text = '法的トラブルの総合解決所である、「法テラス」へご相談されることをご検討ください。'
-                        title = '法テラスのご案内'
-                        subtitle = '法テラスは国が設立した法的トラブルの総合案内所です。'
-                        url_str = 'https://www.houterasu.or.jp/'
-                        image_url = 'http://www.aaaqq.gr.jp/info/media/terasu.gif'
-                        send_message(return_id, text)
-                        send_url_image(return_id, title, subtitle, url_str, image_url)
-
-                    elif message_text == "OK":
-                        text = '弁護士に受理されました。弁護士からの連絡をお待ちください。'
-                        send_message(return_id, text)
-
-                    else:
-                        indexes = tree.search_text(message_text)
-                        if len(indexes) == 1:
-                            text = tree.decide_text(indexes)
-                            send_message(sender_id, text)
-
-                            titles = tree.decide_buttons(indexes)
-                            image_urls = tree.decide_images(indexes)
-                            buttons_titles = []
-                            for title in titles:
-                                button = []
-                                button.append(title)
-                                buttons_titles.append(button)
-                            subtitles = titles
-
-                            send_carousel(sender_id, titles, image_urls, subtitles, buttons_titles)
-
-                        elif len(indexes) == 2:
-                            text = tree.decide_text(indexes)
-                            send_message(sender_id, text)
-
-                            image_urls = tree.decide_images(indexes)
-                            titles = tree.decide_buttons(indexes)
-                            subtitles = titles
-                            send_carousel_buttonless(sender_id, titles, image_urls, subtitles)
-
-                            text = 'アドバイスは役に立ったかな？'
-                            buttons = ['役に立った！']
-                            send_quick_reply(sender_id, text, buttons)
-
     return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text,
-        }
-    })
-
-    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
-
-def send_quick_reply(recipient_id, text, buttons):
-    """
-    :param recipient_id: string
-    :param text: string
-    :param buttons: list; string
-    :return: post
-    """
-
-    params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    quick_replies = []
-
-    for button in buttons:
-        quick_dict = {
-            "content_type": "text",
-            "title": button,
-            "payload": "payload: {}".format(button)
-        }
-        quick_replies.append(quick_dict)
-
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": text,
-            "quick_replies": quick_replies
-        }
-    })
-
-    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
 def send_url_image(recipient_id, title, subtitle, url_str, image_url):
@@ -419,40 +158,6 @@ def send_url_image(recipient_id, title, subtitle, url_str, image_url):
     requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
-def send_image(recipient_id, image_url):
-    """
-    画像を送るだけ
-    :param recipient_id: string: bot送信する相手のID
-    :param title: string: タイトル
-    :param subtitle: string: サブタイトル
-    :param url: string: リンク先のURL
-    :param url_image: string: サムネイル画像が格納されているURL
-    :return: POSTリクエスト
-    """
-
-    params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "image",
-                "payload": {
-                    "url": image_url,
-                    "is_reusable": 'true'
-                }
-            }
-        }
-    })
-
-    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
 
 
 def send_buttons(recipient_id, text, buttons):
@@ -493,111 +198,6 @@ def send_buttons(recipient_id, text, buttons):
                     "template_type": "button",
                     "text": text,
                     "buttons": buttons_postback
-                }
-            }
-        }
-    })
-
-    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
-
-def send_carousel(recipient_id, titles, image_urls, subtitles, buttons_titles):
-    """
-    :param recipient_id: String:
-    :param titles: List: String
-    :param image_urls: List: String
-    :param subtitles: List: String
-    :param buttons_titles: List: List: String
-    :return:
-    """
-
-    params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    elements = []
-    carousel_number = len(titles)
-
-    for num in range(carousel_number):
-
-        buttons = []
-
-        for button_title in buttons_titles[num]:
-            button_dict = {
-                "type": "postback",
-                "title": button_title,
-                "payload": "payload : " + button_title
-            }
-            buttons.append(button_dict)
-
-        carousel_dict = {
-            "title": titles[num],
-            "image_url": image_urls[num],
-            "subtitle": subtitles[num],
-            "buttons": buttons
-        }
-        elements.append(carousel_dict)
-
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements
-                }
-            }
-        }
-    })
-
-    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-
-
-def send_carousel_buttonless(recipient_id, titles, image_urls, subtitles):
-    """
-    :param recipient_id: String:
-    :param titles: List: String
-    :param image_urls: List: String
-    :param subtitles: List: String
-    :param buttons_titles: List: List: String
-    :return:
-    """
-
-    params = {
-        "access_token": ACCESS_TOKEN  # os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    elements = []
-    carousel_number = len(titles)
-
-    for num in range(carousel_number):
-
-        carousel_dict = {
-            "title": titles[num],
-            "image_url": image_urls[num],
-            "subtitle": subtitles[num]
-        }
-        elements.append(carousel_dict)
-
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements
                 }
             }
         }
